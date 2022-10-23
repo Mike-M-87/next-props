71f0dd1d89d7d217c0b7d5acbda6027fa70de67d
CreamyMilk <creamym254@gmail.com>
2022-10-24 02:59:44 +0300

    chore : hard code common noun communities

---
+++ pages/data/communities.ts
@@ -0,0 +1,74 @@
+export const CACHED_COMMUNITY = [
+        {
+            "id": 1,
+            "contractAddress": "0x9c8ff314c9bc7f6e59a9d9225fb22946427edc03",
+            "name": "Nouns",
+            "profileImageUrl": "https://i.imgur.com/seB1iJo.png"
+        },
+        {
+            "id": 2,
+            "contractAddress": "0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B",
+            "name": "Lil Nouns",
+            "profileImageUrl": "https://i.imgur.com/h7rmEOX.png"
+        },
+        {
+            "id": 3,
+            "contractAddress": "0xe169c2ed585e62b1d32615bf2591093a629549b6",
+            "name": "NounPunks",
+            "profileImageUrl": "https://i.imgur.com/nfPXi3e.jpg"
+        },
+        {
+            "id": 5,
+            "contractAddress": "0x898a7dbfddf13962df089fbc8f069fa7ce92cdbb",
+            "name": "Nouns Japan",
+            "profileImageUrl": "https://i.imgur.com/br65CV5.png"
+        },
+        {
+            "id": 7,
+            "contractAddress": "0x79FCDEF22feeD20eDDacbB2587640e45491b757f",
+            "name": "mfers",
+            "profileImageUrl": "https://i.imgur.com/gJ5mO4D.png"
+        },
+        {
+            "id": 8,
+            "contractAddress": "0x2381b67c6f1cb732fdf8b3b29d3260ec6f7420bc",
+            "name": "UMA",
+            "profileImageUrl": "https://i.imgur.com/JhqldL1.png"
+        },
+        {
+            "id": 10,
+            "contractAddress": "0x1CB1A5e65610AEFF2551A50f76a87a7d3fB649C6",
+            "name": "CrypToadz",
+            "profileImageUrl": "https://i.imgur.com/WFoGZ59.png"
+        },
+        {
+            "id": 11,
+            "contractAddress": "0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B",
+            "name": "Lil Golden Globes Awards",
+            "profileImageUrl": "https://i.postimg.cc/RCyMP2Lc/LIL-GOLDEN-GLOBES-prop-house-300.jpg"
+        },
+        {
+            "id": 12,
+            "contractAddress": "0x960b7a6bcd451c9968473f7bbfd9be826efd549a",
+            "name": "OnChainMonkey",
+            "profileImageUrl": "https://i.imgur.com/2zjf3lx.jpg"
+        },
+        {
+            "id": 14,
+            "contractAddress": "0x7c2748C7Ec984b559EADc39C7a4944398E34911a",
+            "name": "The Noun Square",
+            "profileImageUrl": "https://addressform-image-prod.s3.us-east-2.amazonaws.com/6f7d22fe31ea47f8a2a2414a9856aefb/nouny%2520clock%2520logo%2520yellow%2520copy.jpg"
+        },
+        {
+            "id": 15,
+            "contractAddress": "0xF72FAf0050a2cBb645362452a12d46EAdCC09177",
+            "name": "FOODNOUNS",
+            "profileImageUrl": "https://i.imgur.com/4gvH27L.png"
+        },
+        {
+            "id": 16,
+            "contractAddress": "0xbFe00921005F86699760C84C38e3cC86d38745CF",
+            "name": "CoordiNoun Teams",
+            "profileImageUrl": "https://addressform-image-prod.s3.us-east-2.amazonaws.com/6250d4bf1ef2484ba203c096349c51b6/nouny%2520with%2520clipboard%2520coordinoun%2520bigger%2520clipboard.jpg"
+        }
+    ]
